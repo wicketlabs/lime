1bfea0f5f2562dc4f7f114040a19700f42dc2f5f
Kat Schelonka <kat@wicketlabs.com>
2018-08-27 15:31:40 -0700

    only instantiate expensive vars once

--- sparkling_lime/tests/test_discretize.py
+++ sparkling_lime/tests/test_discretize.py
@@ -16,7 +16,10 @@ class TestDiscretize(TestCase):
             "pytest-pyspark-local-testing"))
         cls.sc = SparkContext(conf=conf)
         cls.spark = SparkSession(cls.sc)
+        # Set up expensive variables to reuse
         cls.df = cls._make_data()
+        cls.qd = QuartileDiscretizer(cls.df, ["f0"], cls.df.columns)
+        cls.dd = DecileDiscretizer(cls.df, ["f0"], cls.df.columns)
 
     @classmethod
     def tearDownClass(cls):
@@ -29,7 +32,7 @@ class TestDiscretize(TestCase):
         """
         np.random.seed(42)
         y = np.random.binomial(1, 0.5, n_rows)
-        X = np.zeros((n_rows, 5))
+        X = np.zeros((n_rows, 4))
         z = y - np.random.binomial(1, 0.1, n_rows) \
             + np.random.binomial(1,  0.1, n_rows)
         z[z == -1] = 0
@@ -40,47 +43,44 @@ class TestDiscretize(TestCase):
             np.random.normal(4, 1.2, n_rows)) + np.random.normal(4, 0.1, n_rows)
         X[:, 2] = y + np.random.normal(7.2, 2.8, n_rows)
         X[:, 3] = y ** 2 + np.random.normal(3.9, 1.4, n_rows)
-        X[:, 4] = np.sqrt(y) + np.random.binomial(2, 0.1, n_rows)
 
         if arr:
             return X
         else:
             # Combine data into pyspark dataFrame
-            data = pd.DataFrame(X, columns=["f0", "f1", "f2", "f3", "f4"])
+            data = pd.DataFrame(X, columns=["f0", "f1", "f2", "f3"])
             df = cls.spark.createDataFrame(data)
             return df
 
-    # def test_feature_names(self):
-    #     pass
-    #     # Issue: need to be approximately equal, not equal
-    #
     def test_bin_values(self):
-        data_arr = self._make_data(arr=True)
+        data_arr = self._make_data(n_rows=50, arr=True)
         num_bins = data_arr.shape[1]
-        columns = self.df.columns
         with self.subTest("Quartile Bins are approximately correct"):
             exact_bins = []
             for feature in range(1, num_bins):     # First is categorical
                 qts = np.array(np.percentile(data_arr[:, feature], [25, 50, 75]))
                 exact_bins.append(qts.tolist())
-            qd = QuartileDiscretizer(self.df, ["f0"], columns)
-            approx_bins = qd.bins(self.df, None)
+            approx_bins = self.qd.bins(self.df, None)
             for bins in zip(exact_bins, approx_bins):
                 assert_allclose(bins[0], bins[1], atol=0.5, err_msg=
                                     "Bin values are not close to the exact "
                                     "values.")
+
         with self.subTest("Decile Bins are approximately correct"):
             exact_bins = []
             for feature in range(1, num_bins):     # First is categorical
                 qts = np.array(np.percentile(data_arr[:, feature],
                                              list(range(10, 100, 10))))
                 exact_bins.append(qts.tolist())
-            dd = DecileDiscretizer(self.df, ["f0"], columns)
-            approx_bins = dd.bins(self.df, None)
+            approx_bins = self.dd.bins(self.df, None)
             for bins in zip(exact_bins, approx_bins):
                 assert_allclose(bins[0], bins[1], atol=0.5, err_msg=
                                     "Bin values are not close to the exact "
                                     "values.")
+
+    def test_feature_names(self):
+        pass
+        # TODO (viz): Feature names aren't used since there aren't viz for now
 
     def test_discretizer_consistency(self):
         """
@@ -91,19 +91,17 @@ class TestDiscretize(TestCase):
         self.maxDiff = None
         columns = self.df.columns
         with self.subTest("QuartileDiscretizer has consistent behavior"):
-            qd = QuartileDiscretizer(self.df, ["f0"], columns)
-            disc1 = qd.discretize(self.df)
-            udisc = qd.undiscretize(disc1)
-            disc2 = qd.discretize(udisc)
+            disc1 = self.qd.discretize(self.df)
+            udisc = self.qd.undiscretize(disc1)
+            disc2 = self.qd.discretize(udisc)
             self.assertCountEqual(disc1.select(sorted(disc1.columns)).collect(),
                                   disc2.select(sorted(disc2.columns)).collect(),
                                   "Rows in original discretized dataset do not"
                                   " equal rows in the re-discretized dataset.")
         with self.subTest("DecileDiscretizer has consistent behavior"):
-            dd = DecileDiscretizer(self.df, ["f0"], columns)
-            d_disc1 = dd.discretize(self.df)
-            d_udisc = dd.undiscretize(d_disc1)
-            d_disc2 = dd.discretize(d_udisc)
+            d_disc1 = self.dd.discretize(self.df)
+            d_udisc = self.dd.undiscretize(d_disc1)
+            d_disc2 = self.dd.discretize(d_udisc)
             self.assertCountEqual(
                 d_disc1.select(sorted(d_disc1.columns)).collect(),
                 d_disc2.select(sorted(d_disc2.columns)).collect(),
